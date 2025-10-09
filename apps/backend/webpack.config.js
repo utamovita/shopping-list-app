0447fe8522d415d4228d0c82b5796c0543a23a13
Michał Kowalski <michalkowit@gmail.com>
2025-10-09 20:35:06 +0200

    redeploy #16

---
+++ apps/backend/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.ts',
+  target: 'node',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: './tsconfig.json',
+      }),
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  ignoreWarnings: [
+    {
+      module: /node_modules\/@nestjs\/common\/utils\/load-package\.util\.js/,
+      message:
+        /Critical dependency: the request of a dependency is an expression/,
+    },
+    {
+      module: /node_modules\/@nestjs\/core\/helpers\/load-adapter\.js/,
+      message:
+        /Critical dependency: the request of a dependency is an expression/,
+    },
+  ],
+};
