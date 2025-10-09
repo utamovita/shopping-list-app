155c209744eb17f8f940cc16face0a3979b99f1b
Michał Kowalski <michalkowit@gmail.com>
2025-10-09 16:42:12 +0200

    feat: yet another config changes

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,3 +1,5 @@
+import 'tsconfig-paths/register';
+
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
