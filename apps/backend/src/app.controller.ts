09170d3a0416563b2b65ec6ac1168bfb1ba8cdc9
Michał Kowalski <michalkowit@gmail.com>
2025-10-09 21:07:07 +0200

    redeploy #18

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  //Force redeploy
   @Get()
   getHello(): string {
     return this.appService.getHello();
