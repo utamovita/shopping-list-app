3d547e271f291a5a53239b9e03bf487c8da30a7d
Michał Kowalski <michalkowit@gmail.com>
2025-10-13 20:02:54 +0200

    feat: reverse proxy

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://sharelister-backend.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
