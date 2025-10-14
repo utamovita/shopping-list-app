7b5557792fece8ae6e244182ec63319d1430daa6
Michał Kowalski <michalkowit@gmail.com>
2025-10-14 06:25:08 +0200

    ladies and gentleman, we got him

--- apps/frontend/src/shared/lib/socket/index.ts
+++ apps/frontend/src/shared/lib/socket/index.ts
@@ -11,19 +11,10 @@ const createSocket = (): Socket => {
   }
 
   const URL = env.NEXT_PUBLIC_WS_URL;
-  console.log(`[Socket.IO] Próba połączenia z: ${URL}`);
 
   const instance = io(URL, {
     withCredentials: true,
     transports: ["websocket"],
-  });
-
-  instance.on("connect", () => {
-    console.log(`[Socket.IO] Połączono pomyślnie. ID klienta: ${instance.id}`);
-  });
-
-  instance.on("disconnect", (reason) => {
-    console.log(`[Socket.IO] Rozłączono. Powód: ${reason}`);
   });
 
   instance.on("connect_error", (error) => {
