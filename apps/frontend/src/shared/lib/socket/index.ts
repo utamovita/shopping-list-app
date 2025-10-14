0c31be7f82175855a54030cafeb43a67daad98bb
Michał Kowalski <michalkowit@gmail.com>
2025-10-14 05:59:53 +0200

    deploy to railway #3

--- apps/frontend/src/shared/lib/socket/index.ts
+++ apps/frontend/src/shared/lib/socket/index.ts
@@ -2,6 +2,5 @@ import { io, Socket } from "socket.io-client";
 import { env } from "../env";
 
 export const socket: Socket = io(env.NEXT_PUBLIC_WS_URL, {
-  autoConnect: false,
   withCredentials: true,
 });
